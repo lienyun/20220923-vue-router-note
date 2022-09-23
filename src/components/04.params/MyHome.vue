bfcb76e82d2558954b17027dbfc89f41fdcd4701
Wendy <lienyun.chang@gmail.com>
2022-09-23 16:54:25 +0800

    this.$router.push this.$router.go

--- src/components/04.params/MyHome.vue
+++ src/components/04.params/MyHome.vue
@@ -1,11 +1,17 @@
 <template>
   <h1>MyHome</h1>
+  <button type="button" class="btn btn-primary" @click="goToMovie(3)">導頁到Movie</button>
   <hr>
 </template>
 
 <script>
   export default {
-    name: 'MyHome'
+    name: 'MyHome',
+    methods: {
+      goToMovie(id) {
+        this.$router.push('/movie/' + id)
+      }
+    }
   }
 </script>
 
--- src/components/04.params/MyMovie.vue
+++ src/components/04.params/MyMovie.vue
@@ -1,12 +1,18 @@
 <template>
   <h1>MyMovie ---{{id}}</h1>
   <hr>
+   <button type="button" @click="goBack">上一頁</button>
 </template>
 
 <script>
   export default {
     name: 'MyMovie',
-    props: ['id']
+    props: ['id'],
+    methods: {
+      goBack() {
+        this.$router.go(-1)
+      }
+    }
   }
 </script>
 
